88ef76f3a04ee645c63b6f090e0bbed9c8ec0b75
Tatka404 <tetianaivchyk@gmail.com>
2022-02-07 21:07:03 +0200

    after-review-changes

--- Lesson-3/phone_validation.py
+++ Lesson-3/phone_validation.py
@@ -1,5 +1,5 @@
-phone_number = '380501234567'
-phone_number = [phone_number.isdigit() and len(phone_number) == 12 and phone_number[0:3] == '380']
+phone_number = '38050123459'
+valid_phone_number = phone_number.isdigit() and len(phone_number) == 12 and phone_number[0:3] == '380'
 
-print(phone_number)
+print(valid_phone_number)
 
