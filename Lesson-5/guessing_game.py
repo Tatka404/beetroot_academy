c363b17325ebadf8b1b61cdb49919940ce2f4113
Tatka404 <tetianaivchyk@gmail.com>
2022-02-19 02:11:10 +0200

    quotes/typo/elif-condition

--- Lesson-5/guessing_game.py
+++ Lesson-5/guessing_game.py
@@ -1,11 +1,13 @@
 import random
 
 random_number = random.randint(1, 10)
-player = int(input('Let\'s start? Guess any number from 1 to 10: '))
+player_guess = int(input("Let's start? Guess any number from 1 to 10: "))
 
-if player > 10 or player < 1:
-    print('Your input is out of scope! >_<')
-elif not player == random_number:
-    print('Try again... :(')
-elif player == random_number:
-    print('You are a wisard! ^_^')
+if player_guess < 1 or player_guess > 10:
+    print("Your input is out of scope! >_<")
+elif player_guess == random_number:
+    print("You are a wizard! ^_^")
+else:
+    print("Try again... :(")
+
+
