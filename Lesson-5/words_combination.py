19ac79e51fd40ef1f429ef8227ddaa51efbd9107
Tatka404 <tetianaivchyk@gmail.com>
2022-02-19 02:14:23 +0200

    variables-named

--- Lesson-5/words_combination.py
+++ Lesson-5/words_combination.py
@@ -2,7 +2,6 @@ from random import choice
 
 given_word = input('Your word: ')
 
-result = [''.join(choice(given_word) for _ in range(len(given_word))) for _ in range(5)]
+result = [''.join(choice(given_word) for x in range(len(given_word))) for y in range(5)]
 
 print(f'Your random words are {result}')
-
