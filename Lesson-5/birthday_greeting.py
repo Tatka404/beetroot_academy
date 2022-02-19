71333075f56e4cd0db135dafc63ee7f20e37b96f
Tatka404 <tetianaivchyk@gmail.com>
2022-02-19 02:04:10 +0200

    string-quotes-fixed

--- Lesson-5/birthday_greeting.py
+++ Lesson-5/birthday_greeting.py
@@ -2,5 +2,5 @@ name = input('What is your name? ')
 age = int(input('How old are you? '))
 next_year_age = age + 1
 
-print(f'Hello {name}, on your next birthday you\'ll be {next_year_age} years.'
-      f'\nP.S. If you\'ll survive in this cruel world')
+print(f"Hello {name}, on your next birthday you'll be {next_year_age} years."
+      f"\nP.S. If you'll survive in this cruel world.")
