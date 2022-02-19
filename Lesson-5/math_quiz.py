c5dd40bf8cd8a0563e697033f0c1dd261cf11e91
Tatka404 <tetianaivchyk@gmail.com>
2022-02-19 02:12:59 +0200

    rename-variable

--- Lesson-5/math_quiz.py
+++ Lesson-5/math_quiz.py
@@ -1,11 +1,10 @@
 first_value = 2
 second_value = 2
-quiz_question = first_value + second_value
+quiz_answer = first_value + second_value
 
 player_answer = float(input(f'How much is {first_value} + {second_value}? '))
 
-if player_answer == quiz_question:
+if player_answer == quiz_answer:
     print('You are a genius!')
 else:
     print('Oh no ...')
-
